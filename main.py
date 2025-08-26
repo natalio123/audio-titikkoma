58883b03a27291bf907e9a84d538f77638d8575d
Yonathan Sihotang <143815443+sihotang-yonathan1@users.noreply.github.com>
2025-08-26 23:49:05 +0800

    feat: Add more data and save it to new dataset

--- main.py
+++ main.py
@@ -15,14 +15,28 @@ def main():
     
     # for index, url in enumerate(video_urls, start=1):
     #     extract_audio(index, url)
-
+    _new_row = []
     for _, row in df.iterrows():
         row_id = row['id']
         video_url = row['video']
-        extract_audio(row_id, video_url)
+        info = extract_audio(row_id, video_url)
 
-        if row_id >= 2:
-            break
+        print(f'dataset_id: {row_id}: Trying to append more data')
+        _new_row.append({
+            "id": row_id,
+            "video": video_url,
+            "title": info["title"],
+            "description": info["description"],
+            "duration": info["duration"],
+            "like_count": info["like_count"],
+            "timestamp": info["timestamp"],
+            "audio_codec": info["acodec"],
+            "audio_bitrate": info["abr"],
+            "audio_sampling_rate": info["asr"],
+        })
+
+    new_df = pd.DataFrame(_new_row)
+    new_df.to_csv('./data/temp_new_df.csv')
 
 if __name__ == "__main__":
     main()
